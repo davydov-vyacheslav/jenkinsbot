ee70f46bdb3d3cc762da5ecdb05fe1c1b98567bd
Davydov V <vyacheslav.v.davydov@gmail.com>
2022-05-08 19:29:52 +0300

    cleanup and docker removal poc

--- src/main/java/com/javanix/bot/jenkinsBot/database/settings/SettingsRepository.java
+++ src/main/java/com/javanix/bot/jenkinsBot/database/settings/SettingsRepository.java
@@ -1,11 +1,9 @@
 package com.javanix.bot.jenkinsBot.database.settings;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 interface SettingsRepository extends MongoRepository<SettingsEntity, String> {
 
 	Optional<SettingsEntity> findByKeyIgnoreCase(String key);
